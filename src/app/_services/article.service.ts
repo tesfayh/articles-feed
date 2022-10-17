db50c77b372c88348cdd8656b4c33b9dd6b79fa0
Tesfay Hishe <tesfayh09@gmail.com>
2022-10-17 10:57:58 +0300

    api key header added to service

--- src/app/_services/article.service.ts
+++ src/app/_services/article.service.ts
@@ -10,6 +10,8 @@ export class ArticleService {
     constructor(private http: HttpClient) { }
 
   getArticles(): Observable<any>{
-    return this.http.get<[]>(this.baseUrl);
+    const headers = new HttpHeaders()
+    .set('X-Api-Key', 'a6f0643366f7462cbae55c7ba5dc7d01');
+    return this.http.get<[]>(this.baseUrl, { 'headers': headers });
   }
 }
